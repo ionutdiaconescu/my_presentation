a43a8db4b5b09a996956bd51665b80e7b8533720
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2023-03-26 22:42:46 +0300

    Update-CV, update links to gihub and apps

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const WorkItems = ({ item }) => {
   return (
     <div className="work__card" key={item.id}>
-      <img src={item.image} alt="" className="work__img" />
+      <a href={item.linkapp} target="_blank" rel="noreferres">
+        <img src={item.image} alt="" className="work__img" />
+      </a>
       <h3 className="work__title">{item.title}</h3>
+
       <a
-        href="https://github.com/ionutdiaconescu?tab=repositories"
+        href={item.githublink}
         target="_blank"
         rel="noreferrer"
         className="work__button"
